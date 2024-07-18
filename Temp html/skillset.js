6aefcf68836f2ce08c90dbcfa1339e27ac961588
ALIGHT22 <97866408+ALIGHT22@users.noreply.github.com>
2024-07-18 19:38:40 -0400

    Create skillset.js

---
+++ Temp html/skillset.js
@@ -0,0 +1,9 @@
+
+function saveSkill() {
+    alert('Skill information saved!');
+    // Add code to save skill data
+}
+
+function closeForm(formId) {
+    document.getElementById(formId).style.display = 'none';
+}
