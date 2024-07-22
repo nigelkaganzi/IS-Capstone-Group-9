d003b67c989cdb7fd7c9e7c8469b9bee39582d5f
nigelkaganzi <142240454+nigelkaganzi@users.noreply.github.com>
2024-07-21 21:17:39 -0400

    FInal changes
    asdf

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 class Config:
     SECRET_KEY = 'ISCAPSTONEGROUP9'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password123@localhost/capstoneproject'
-    SQLALCHEMY_DATABASE_URI = 'postgres://u4rfb3cisarrpt:p9d62a4a5fba32ec58678cd0d01bdc598d01107b0f8a6183437d3c895a29a40f9@c5hilnj7pn10vb.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/ded9nc7r3ao5fg'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password123@localhost/capstoneproject'
+    #SQLALCHEMY_DATABASE_URI = 'postgres://u4rfb3cisarrpt:p9d62a4a5fba32ec58678cd0d01bdc598d01107b0f8a6183437d3c895a29a40f9@c5hilnj7pn10vb.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/ded9nc7r3ao5fg'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_PERMANENET = False
